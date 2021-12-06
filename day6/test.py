088036e7dfe8ff5c6de4cf68a0ce119f8df370aa
Sankalp Patil <patilsa@oregonstate.edu>
2021-12-05 21:49:51 -0800

    day 6

---
+++ day6/test.py
@@ -0,0 +1,25 @@
+with open('Lanternfish.txt') as f:
+    data = [x.strip() for x in f]
+    data = data[0].split(',')
+    data = [int(x) for x in data]
+
+print(data)
+
+for i in range(80):
+
+    fishToAdd = []
+
+    for fish in range(len(data)):
+        if data[fish] > 0:
+            data[fish] -= 1
+
+        elif data[fish] == 0:
+            data[fish] = 6
+            fishToAdd.append(8)
+
+    for fish in fishToAdd:
+        data.append(fish)
+
+
+print(len(data))
+
---
+++ day6/test2.py
@@ -0,0 +1,27 @@
+
+with open('test.txt', 'r') as f:
+
+    data = f.readline().strip().split(',')
+    data = list(map(int, data))
+
+    # keeps track of how many fish are in each stage of life
+    buckets = [0 for _ in range(10)]
+
+    for fish_life in data:
+        buckets[fish_life] += 1
+
+    total_days = 256
+    for day in range(total_days):
+        for idx, count in enumerate(buckets):
+            if idx == 0:
+                # we use 7 because the enumerate will decrease it to 6 in the current iter.
+                buckets[7] += count
+                buckets[9] += count
+                buckets[idx] = 0
+            else:
+                buckets[idx - 1] += count
+                buckets[idx] = 0
+
+    print(sum(buckets))
+
+
